f44b5daaa8731d8abe236ab073d104bf758fa61b
chandru-mcw <chandru.kumaresan@multicorewareinc.com>
2026-04-28 14:38:09 +0530

    ADD : MSYS2-runtime-aarch64-stage-2 PKGBUILD

---
+++ msys2-runtime-aarch64-stage-2/build-msys.sh
@@ -0,0 +1,43 @@
+!/bin/bash
+set -e  # stop if any command fails
+
+ROOT_DIR=$(pwd)
+
+echo "===== STEP 1: Building NEWLIB ====="
+cd "$ROOT_DIR/newlib_pkgbuild"
+
+
+rm -rf src pkg
+makepkg -o
+
+
+cd src/msys2-runtime/newlib
+
+find . -name "Makefile.in" -delete
+find . -name "Makefile" -delete
+
+autoreconf -fvi
+
+cd ../../..
+cp -rf  src/msys2-runtime/newlib/libc/include/getopt.h .
+cp -rf src/msys2-runtime/winsup/cygwin/include/* src/msys2-runtime/newlib/libc/include/ 
+cp -rf ./getopt.h src/msys2-runtime/newlib/libc/include/
+makepkg -e
+
+echo "===== NEWLIB BUILD DONE ====="
+
+
+echo "===== STEP 2: Building WINSUP (msys2-runtime) ====="
+cd "$ROOT_DIR"
+
+# # Clean + build
+cp -rf newlib_pkgbuild/msys2-runtime/ .
+cp -rf newlib_pkgbuild/pkg/ .
+cp -rf newlib_pkgbuild/src/ .
+
+rm -rf src/runtime-build
+cp -rf newlib_pkgbuild/src/runtime-build/aarch64-pc-msys/newlib  src/.
+
+makepkg -e
+
+echo "===== ALL BUILDS COMPLETED SUCCESSFULLY ====="
